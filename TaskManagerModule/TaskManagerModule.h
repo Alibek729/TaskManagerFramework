22c3308a709f2fb02a1ffbc8c772fa38d02c34ec
Alibek Kozhambekov <kozhambekovalibek@gmail.com>
2023-06-17 19:47:32 +0600

    Task manager module hes been successfully separated from app and now can be added as framework

---
+++ TaskManagerModule/TaskManagerModule.h
@@ -0,0 +1,18 @@
+//
+//  TaskManagerModule.h
+//  TaskManagerModule
+//
+//  Created by Alibek Kozhambekov on 17.06.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TaskManagerModule.
+FOUNDATION_EXPORT double TaskManagerModuleVersionNumber;
+
+//! Project version string for TaskManagerModule.
+FOUNDATION_EXPORT const unsigned char TaskManagerModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TaskManagerModule/PublicHeader.h>
+
+
